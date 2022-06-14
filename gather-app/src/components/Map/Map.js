f99a119f417e27cb83710bcc7ac797b556d3346c
Jun <junsoopark92@gmail.com>
2022-06-14 13:32:30 -0400

    update google map

--- gather-app/src/components/Map/Map.js
+++ gather-app/src/components/Map/Map.js
@@ -1,6 +1,5 @@
 import React, {useState, useCallback, useEffect} from 'react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
-import Geocode from "react-geocode";
 
 const containerStyle = {
   width: '100%',
@@ -18,34 +17,14 @@ const options = {
 
 function Map(props) {
 
-  // const [map, setMap] = useState(null);
-
-  const { isLoaded, loadError } = useJsApiLoader({
+  const isLoaded = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyAwcOSQ6hnqoqiXX_1D1ykHOBAZZ2UorHE"
   })
 
-  if(loadError) return "Error loading maps"
-  if(!isLoaded) return "Loading Maps"
-
-  // const onLoad = useCallback(function callback(map) {
-  //   const bounds = new window.google.maps.LatLngBounds(center);
-  //   //map.fitBounds(bounds);
-  //   setMap(map)
-  // }, [])
-
-  const onLoad = marker => {
-    console.log('marker: ', marker)
-  }
-
-  // const onUnmount = useCallback(function callback(map) {
-  //   setMap(null)
-  // }, [])
-
   const coordinatesList = props.postList.map((post) => {
     return (
       <Marker
-      onLoad={onLoad}
       position= {{lat: post.coordinates.lat, lng: post.coordinates.lng}} 
       icon={{
         url:"https://i.ibb.co/4JFPCZP/location-dot-solid.png",
@@ -60,12 +39,10 @@ function Map(props) {
         mapContainerStyle={containerStyle}
         center={center}
         zoom={11}
-        // onLoad={onLoad}
-        // onUnmount={onUnmount}
         options={options}
       >
 
-      {coordinatesList} 
+      await {coordinatesList} 
 
       </GoogleMap>
   ) : <h1>Loading</h1>
